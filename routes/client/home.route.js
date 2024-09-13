a1164c224a8080252530208b3112d12acb2e6883
Phamducvinh <phamducvinh2k3@gmail.com>
2024-09-13 07:35:38 +0700

    tạo layout và kết nối database mongodb

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render("client/pages/home/index");
-});
+
+
+const controller = require('../../controllers/client/home.controller');
+
+router.get('/', controller.index);
 
 module.exports = router;
