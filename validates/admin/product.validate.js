825ed3b66fa5806ff7153c42824416760270437d
phamvinh203 <vinhnd203@gmail.com>
2024-11-07 22:28:09 +0700

    add validates

---
+++ validates/admin/product.validate.js
@@ -0,0 +1,8 @@
+module.exports.createPost = (req, res, next) => {
+    if(!req.body.title){
+        req.flash("error", "Vui lòng nhập tiêu đề!");
+        return res.redirect("back");
+    }
+    
+    next();
+}
