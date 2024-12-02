d717eb5c6593ae17e88a6ad2f943028e50845ece
VinhPham <95126697+Phamducvinh@users.noreply.github.com>
2024-12-02 22:02:17 +0700

    thêm chi tiết sản phầm (admin)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -3,7 +3,7 @@ const Product = require("../../models/product.model");
 // [get] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
-        status: "available",
+        status: "active",
         deleted: false
     }).sort({ position: "desc" });
 
